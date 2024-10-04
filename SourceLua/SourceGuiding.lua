3d51c78b72198c510fb4d07777b5ebdbdefc7ee7
.unitz01 <167648625+lucianavfxdude@users.noreply.github.com>
2024-10-04 17:45:50 -0400

    Update SourceGuiding.lua

--- SourceLua/SourceGuiding.lua
+++ SourceLua/SourceGuiding.lua
@@ -85,6 +85,7 @@ function Crucifix(model, playerTool, config)
 	local entityPart = repentance.Entity
 	local sound = (config.Resist and crucifix.SoundFail or crucifix.Sound)
 	local crash = repentance.Crucifix.Crash
+	local zipzap = repentance.Crucifix.Assemble
 	local shaker = moduleScripts.Main_Game.camShaker:StartShake(5, 20, 2, Vector3.new())
 
     -- Repentance setup
@@ -95,6 +96,8 @@ function Crucifix(model, playerTool, config)
 	repentance.Parent = workspace
 	sound:Play()
 	crash:Play()
+	wait(0.1)
+	zipzap:Play()
 
     -- Teleport model to repentance entity part
 	task.spawn(function()
