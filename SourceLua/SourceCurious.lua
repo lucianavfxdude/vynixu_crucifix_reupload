8654ac8a96f66cea05a8452394722d88eeb15db7
.unitz01 <167648625+lucianavfxdude@users.noreply.github.com>
2024-10-11 18:42:55 -0400

    Update SourceCurious.lua

--- SourceLua/SourceCurious.lua
+++ SourceLua/SourceCurious.lua
@@ -86,6 +86,7 @@ function Crucifix(model, playerTool, config)
 	local sound = (config.Resist and crucifix.SoundFail or crucifix.Sound)
 	local crash = repentance.Crucifix.Crash
 	local warble = repentance.Crucifix.Warble
+	local straytool = workspace.Crucifix
 	local shaker = moduleScripts.Main_Game.camShaker:StartShake(5, 20, 2, Vector3.new())
 
     -- Repentance setup
@@ -96,6 +97,7 @@ function Crucifix(model, playerTool, config)
 	repentance.Parent = workspace
 	sound:Play()
 	warble:Play()
+	straytool:Destroy()
 
     -- Teleport model to repentance entity part
 	task.spawn(function()
