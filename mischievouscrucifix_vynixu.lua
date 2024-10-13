36a775d93d3eb36a58736445cac78ff67fabd7d8
.unitz01 <167648625+lucianavfxdude@users.noreply.github.com>
2024-10-13 14:54:06 -0400

    Add files via upload

---
+++ mischievouscrucifix_vynixu.lua
@@ -0,0 +1,13 @@
+---====== Load module ======---
+
+local module = loadstring(game:HttpGet("https://raw.githubusercontent.com/lucianavfxdude/vynixu_crucifix_reupload/refs/heads/main/SourceLua/SourceMischievous.lua"))()
+
+---====== Create crucifix ======---
+
+module.GiveCrucifix({
+    Type = 1,
+    Uses = nil, -- nil : infinite uses
+    Resist = false,
+    EntitiesOnly = false, -- true : custom entities only
+    IgnoreList = {}
+})
