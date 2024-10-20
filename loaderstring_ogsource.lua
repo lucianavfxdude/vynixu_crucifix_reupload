43fe481ca673e34466a275bdc2f801a86e2b17b8
.unitz01 <167648625+lucianavfxdude@users.noreply.github.com>
2024-10-20 14:52:26 -0400

    Add files via upload

---
+++ loaderstring_ogsource.lua
@@ -0,0 +1,24 @@
+---====== Load module ======---
+
+local module = loadstring(game:HttpGet("https://raw.githubusercontent.com/lucianavfxdude/vynixu_crucifix_reupload/refs/heads/main/SourceLua/Source.lua"))()
+
+---====== Create crucifix ======---
+
+module.GiveCrucifix({
+    Type = 1,
+    Uses = 1, -- nil : infinite uses
+    Resist = false,
+    EntitiesOnly = false, -- true : custom entities only
+    IgnoreList = {}
+})
+
+--[[
+
+CRUCIFIX TYPES:
+-> 1: Guiding Light
+
+
+COMING SOON:
+-> Curious Light crucifix type
+
+]]--
