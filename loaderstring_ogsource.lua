f7bb87285f26752db42ea307532a3779aa0b9bc1
.unitz01 <167648625+lucianavfxdude@users.noreply.github.com>
2024-10-23 15:40:22 -0400

    Update loaderstring_ogsource.lua

--- loaderstring_ogsource.lua
+++ loaderstring_ogsource.lua
@@ -1,31 +1,31 @@
--- ---====== Load module ======---
+---====== Load module ======---
 
--- local module = loadstring(game:HttpGet("https://raw.githubusercontent.com/lucianavfxdude/vynixu_crucifix_reupload/refs/heads/main/SourceLua/Source.lua"))()
+local module = loadstring(game:HttpGet("https://raw.githubusercontent.com/lucianavfxdude/vynixu_crucifix_reupload/refs/heads/main/SourceLua/Source.lua"))()
 
--- -- -====== Create crucifix ======---
+-- -====== Create crucifix ======---
 
--- module.GiveCrucifix({
---     Type = 1,
---     Uses = 1, -- nil : infinite uses
---     Resist = false,
---     EntitiesOnly = false, -- true : custom entities only
---     IgnoreList = {}
--- })
+module.GiveCrucifix({
+    Type = 1,
+    Uses = nil, -- nil : infinite uses
+    Resist = false,
+    EntitiesOnly = false, -- true : custom entities only
+    IgnoreList = {}
+})
 
---[[
+-- [[
 
-CRUCIFIX TYPES:
--> 1: Guiding Light
+-- CRUCIFIX TYPES:
+-- -> 1: Guiding Light
 
 
-COMING SOON:
--> Curious Light crucifix type
+-- COMING SOON:
+-- -> Curious Light crucifix type
 
-]]--
+-- ]]--
 
-_G.Uses = 414141414141
-_G.Range = 20
-_G.OnAnything = true
-_G.Fail = false
--- _G.Variant = "Electric"
-loadstring(game:HttpGet('https://raw.githubusercontent.com/PenguinManiack/Crucifix/main/Crucifix.lua'))()
+-- _G.Uses = 414141414141
+-- _G.Range = 20
+-- _G.OnAnything = true
+-- _G.Fail = false
+-- -- _G.Variant = "Electric"
+-- loadstring(game:HttpGet('https://raw.githubusercontent.com/PenguinManiack/Crucifix/main/Crucifix.lua'))()
