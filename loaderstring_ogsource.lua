eba066df702f2e5a6a422d6e3d8383f8cb694d4d
.unitz01 <167648625+lucianavfxdude@users.noreply.github.com>
2024-10-23 19:48:13 -0400

    Update loaderstring_ogsource.lua

--- loaderstring_ogsource.lua
+++ loaderstring_ogsource.lua
@@ -1,14 +1,14 @@
 ---====== Load module ======---
 
-local module = loadstring(game:HttpGet("https://raw.githubusercontent.com/lucianavfxdude/vynixu_crucifix_reupload/refs/heads/main/SourceLua/SourcePlayful.lua"))()
+local module = loadstring(game:HttpGet("https://raw.githubusercontent.com/lucianavfxdude/vynixu_crucifix_reupload/refs/heads/main/SourceLua/Source.lua"))()
 
 -- -====== Create crucifix ======---
 
 module.GiveCrucifix({
     Type = 1,
-    Uses = nil, -- nil : infinite uses
+    Uses = 1, -- nil : infinite uses
     Resist = false,
-    EntitiesOnly = false, -- true : custom entities only
+    EntitiesOnly = true, -- true : custom entities only
     IgnoreList = {}
 })
 
