538610557dd1a824cb970b3220c2eff7e5367a6c
.unitz01 <167648625+lucianavfxdude@users.noreply.github.com>
2024-10-26 14:19:33 -0400

    Update SourceCurious.lua

--- SourceLua/SourceCurious.lua
+++ SourceLua/SourceCurious.lua
@@ -20,7 +20,7 @@ local localMouse = localPlayer:GetMouse()
 
 local assets = {
     Repentance = LoadCustomInstance("https://github.com/lucianavfxdude/vynixu_crucifix_reupload/blob/main/Assets/CrucifixBases/CuriousRepentance.rbxm?raw=true"),
-    Crucifix = LoadCustomInstance("https://github.com/lucianavfxdude/vynixu_crucifix_reupload/raw/refs/heads/main/Assets/CrucifixModels/gutcruc.rbxm")
+    Crucifix = LoadCustomInstance("https://github.com/lucianavfxdude/vynixu_crucifix_reupload/raw/refs/heads/main/Assets/CrucifixModels/rippedcrucifix.rbxm")
 }
 local moduleScripts = {
 	Main_Game = require(localPlayerGui.MainUI.Initiator.Main_Game),
